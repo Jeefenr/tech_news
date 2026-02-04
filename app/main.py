71da4d8e267e6cfdb0c91d2b80e7399636860a7a
Sever <mesevra02101976@gmail.com>
2026-02-04 08:44:46 +0200

    Add project structure and basic Flask app

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Фейкові новини для тесту
+dummy_news = [
+    {"title": "Python 3.13 вийшов!", "source": "Official", "published": "2026-01-25"},
+    {"title": "Штучний інтелект замінить програмістів?", "source": "TechBlog", "published": "2026-01-26"}
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', news=dummy_news)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
